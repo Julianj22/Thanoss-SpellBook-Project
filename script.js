b7dcdd21c9f27402f5bb9996ae16ba8ab3a19e0e
Julianj22 <42346111+Julianj22@users.noreply.github.com>
2019-01-02 12:07:52 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,35 @@
+//Spell #1
+
+$("#power").click(function(){
+ $("#iron_man").hide();
+  $("#iron_man2").show();
+});
+
+
+
+//Spell #2
+
+$("#space").click(function(){
+ $("#universe").fadeToggle();
+});
+
+
+
+//Spell #3
+
+$("#reality").click(function(){
+ $("#home").slideToggle();
+});
+
+$("#soul").click(function(){
+ $("#peter").fadeOut();
+  $("#sand").fadeIn();
+});
+
+$("#time").click(function(){
+ $("#win").slideUp();
+  $("#yeet").fadeIn();
+});
+
+
+
